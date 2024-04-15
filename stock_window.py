bb2715106840bad7560bc692580ee8276bfd05be
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-04-15 10:16:11 +0200

    Update stock_window.py

--- stock_window.py
+++ stock_window.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import yfinance as yf
 from matplotlib import style
+import argparse
+import threading
 import warnings
+import os
 warnings.filterwarnings("ignore")
 
 style.use('dark_background')
@@ -12,6 +15,11 @@ root = tk.Tk()
 root.title("Stock Window")
 root.configure(background="gray")
 root.geometry("900x650")
+
+current_dir = tk.StringVar()
+current_dir.set(os.getcwd())
+
+tk.Entry(root, textvariable=current_dir,state="readonly").pack(side=tk.TOP,fill=tk.X)
 
 
 root.mainloop()
